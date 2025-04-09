d5b7e58ac6847a71ba25223cb0bdb3ea35fda2f5
Spinon64 <spinon@ucol.mx>
2025-04-09 09:07:59 -0600

    Correccion en NavBar max-w

--- src/Components/Organisms/NavBar.jsx
+++ src/Components/Organisms/NavBar.jsx
@@ -43,7 +43,7 @@ const NavBar = () => {
     >
       {({ open }) => (
         <>
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="max-w-9xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between items-center h-16">
               {/* Logo y botón de menú */}
               <div className="flex items-center">
